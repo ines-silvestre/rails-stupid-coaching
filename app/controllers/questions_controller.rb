1570908844789ee5277273facf853bd0c230561e
Inès Silvestre <110823086+ines-silvestre@users.noreply.github.com>
2022-11-14 18:57:05 +0100

    exo2done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,18 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+
+    if @question == 'hello'
+      @answer = 'Hi Little Bird !'
+    elsif @question == 'what time is it ?'
+      @answer = "We are precisely the #{Time.now}"
+    elsif @question == 'i am going to work'
+      @answer = 'Great !'
+    elsif @question.ends_with?('?')
+     @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don t care, get dressed and go to work!"
+    end
   end
 end
